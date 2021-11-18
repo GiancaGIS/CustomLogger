caf94dab8ac386729a56cb71429aa275431773fd
OVERIT\giancarlo.messina <giancarlo.messina@overit.it>
2021-11-18 16:27:35 +0100

    Raffinato sync e async

--- CustomLogger/Logger.cs
+++ CustomLogger/Logger.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-
 
 namespace CustomLogger
 {
     internal class FileLogger
     {
         private readonly string pathFile = string.Empty;
-        private readonly object objLock = new Object();
+        private readonly object objLock = new object();
+        private List<Task> listaTask = null;
 
-        public FileLogger(string path)
+        public FileLogger(string path, ref List<Task> tasks)
         {
             this.pathFile = path;
+            this.listaTask = tasks;
         }
 
         public void LogSync(string messaggio, string intestazione)
@@ -26,6 +28,7 @@ namespace CustomLogger
 
         public void LogAsync(string messaggio, string intestazione)
         {
+            this.listaTask.Add(
             new Task(() =>
             {
                 try
@@ -54,7 +57,7 @@ namespace CustomLogger
                 {
                     Monitor.Exit(objLock);
                 }
-            }, TaskCreationOptions.None).Wait();
+            }, TaskCreationOptions.None));
         }
     }
 
@@ -62,20 +65,51 @@ namespace CustomLogger
     {
         private readonly string pathFile = string.Empty;
         private readonly bool isAsync = false;
+        private readonly bool startTask = false;
+        private FileLogger logger = null;
 
-        public LogHelper(string path, bool async = false)
+        /// <summary>
+        /// Lista di tutti i task di scrittura dei log in modalità async, dovranno essere avviati
+        /// e aspettati con apposito metodo EseguiTuttiTaskScritturaLog
+        /// </summary>
+        public List<Task> ListaTaskScritturaLog = new List<Task>();
+
+
+        /// <summary>
+        /// Costruttore della classe
+        /// </summary>
+        /// <param name="path">Path completa al file di Log</param>
+        /// <param name="async">True per scrivere in modalità async</param>
+        /// <param name="startTaskIfAsync"></param>
+        public LogHelper(string path, bool async = false, bool startTaskIfAsync = false)
         {
             this.isAsync = async;
+            this.startTask = startTaskIfAsync;
+
+            if (!this.isAsync) this.startTask = false;
 
             if (!Directory.Exists(System.IO.Path.GetDirectoryName(path)))
                 throw new ArgumentException("La path al file specificato non esiste!");
 
             if (string.IsNullOrEmpty(System.IO.Path.GetFileName(path)))
-                throw new ArgumentException("Non è stato specificato il nome del file txt!");
+                throw new ArgumentException("Non è stato specificato il nome del file di log!");
 
             this.pathFile = path;
         }
-        private FileLogger logger = null;
+
+
+        /// <summary>
+        /// Esegue i task di scrittura di tutti i log in modalità async da scrivere
+        /// </summary>
+        public void EseguiTuttiTaskScritturaLog()
+        {
+            this.ListaTaskScritturaLog.ForEach(task =>
+            {
+                task.Start();
+                task.Wait();
+            });
+
+        }
 
         /// <summary>
         /// Restituisce task, dove il result della string è l'eventuale messaggio di errore
@@ -84,29 +118,46 @@ namespace CustomLogger
         /// <returns></returns>
         public void LogInfo(string message)
         {
-            logger = new FileLogger(this.pathFile);
-            if (this.isAsync) logger.LogAsync(message, $"[{DateTime.Now}]|Info|:");
+            logger = new FileLogger(this.pathFile, ref this.ListaTaskScritturaLog);
+            if (this.isAsync)
+            {
+                logger.LogAsync(message, $"[{DateTime.Now}]|Info|:");
+
+                if (this.startTask) this.EseguiTuttiTaskScritturaLog();
+            }
             else logger.LogSync(message, $"[{DateTime.Now}]|Info|:");
         }
 
         public void LogDebug(string message)
         {
-            logger = new FileLogger(this.pathFile);
-            if (this.isAsync) logger.LogAsync(message, $"[{DateTime.Now}]|Debug|:");
+            logger = new FileLogger(this.pathFile, ref this.ListaTaskScritturaLog);
+            if (this.isAsync)
+            {
+                logger.LogAsync(message, $"[{DateTime.Now}]|Debug|:");
+                if (this.startTask) this.EseguiTuttiTaskScritturaLog();
+            }
             else logger.LogSync(message, $"[{DateTime.Now}]|Debug|:");
         }
 
         public void LogWarning(string message)
         {
-            logger = new FileLogger(this.pathFile);
-            if (this.isAsync) logger.LogAsync(message, $"[{DateTime.Now}]|Warning|:");
+            logger = new FileLogger(this.pathFile, ref this.ListaTaskScritturaLog);
+            if (this.isAsync)
+            {
+                logger.LogAsync(message, $"[{DateTime.Now}]|Warning|:");
+                if (this.startTask) this.EseguiTuttiTaskScritturaLog();
+            }
             else logger.LogSync(message, $"[{DateTime.Now}]|Warning|:");
         }
 
         public void LogError(string message)
         {
-            logger = new FileLogger(this.pathFile);
-            if (this.isAsync) logger.LogAsync(message, $"[{DateTime.Now}]|Error|:");
+            logger = new FileLogger(this.pathFile, ref this.ListaTaskScritturaLog);
+            if (this.isAsync)
+            {
+                logger.LogAsync(message, $"[{DateTime.Now}]|Error|:");
+                if (this.startTask) this.EseguiTuttiTaskScritturaLog();
+            }
             else logger.LogSync(message, $"[{DateTime.Now}]|Error|:");
         }
     }
